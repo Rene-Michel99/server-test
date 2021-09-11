027634cd6c68f1c03b87482ba8b9a6ed37d36d2a
Rene-Michel99 <renemichel856@gmail.com>
2021-09-11 13:09:43 -0300

    delete

--- db/db.js
+++ db/db.js
@@ -39,11 +39,12 @@ class Database {
             return "empty";
         }else{
             //for(var i = 0; i < response.length; i++){
-                //await this.model.message.destroy({where:{id:response[i].id}});
+                //
             //}
             var data = [];
             for(var i = 0; i < response.length; i++){
                 data.push({created:response[i].createdAt});
+                await this.model.message.destroy({where:{id:response[i].id}});
             }
             return data;
         }
